bd926b9fa65e6c208d92220e269f9fad8141fac5
YaroslavTyrtyshnyi <148007608+zeengy@users.noreply.github.com>
2024-06-19 14:17:23 +0300

    Update Program.cs

--- Lab12/Program.cs
+++ Lab12/Program.cs
@@ -1,37 +1,76 @@
-﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace Lab12
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static string[] ReadFromFile(string filename)
         {
-
-            string[] ReadFromFile(string filename)
+            try
             {
-                try
+                string text = System.IO.File.ReadAllText(filename);
+                int wordCount = CountWords(text);
+
+                string[] words = new string[wordCount];
+                int wordIndex = 0;
+                int startIndex = 0;
+
+                for (int i = 0; i <= text.Length; i++)
                 {
-                    return File.ReadAllLines(filename);
+                    if (i == text.Length || text[i] == ' ' || text[i] == '.' || text[i] == ',')
+                    {
+                        int length = i - startIndex;
+                        words[wordIndex++] = text.Substring(startIndex, length);
+                        startIndex = i + 1;
+                    }
                 }
-                catch (Exception ex)
+
+                return words;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading file: {ex.Message}");
+                return new string[0];
+            }
+        }
+
+        static int CountWords(string text)
+        {
+            int count = 0;
+            bool inWord = false;
+
+            foreach (char c in text)
+            {
+                if (char.IsWhiteSpace(c) || c == '.' || c == ',')
                 {
-                    return new string[0];
+                    if (inWord)
+                    {
+                        count++;
+                        inWord = false;
+                    }
+                }
+                else
+                {
+                    if (!inWord)
+                    {
+                        inWord = true;
+                    }
                 }
             }
 
+            if (inWord)
+            {
+                count++;
+            }
 
-            string[] textFile = ReadFromFile("input.txt");
-            string text = textFile[0];
+            return count;
+        }
 
-            string[] words = text.Split(new char[] { ' ', '.', ',' });
+        static void Main(string[] args)
+        {
+            string[] words = ReadFromFile("input.txt");
 
-            int wordCount = words.Length;
-            Console.WriteLine("Кількість слів у тексті: " + wordCount);
+            Console.WriteLine("Кількість слів у тексті: " + words.Length);
 
             Array.Sort(words);
             Console.WriteLine("Слова у тексті за абеткою:");
@@ -42,3 +81,4 @@ namespace Lab12
         }
     }
 }
+
